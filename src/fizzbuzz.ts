32a951d33f23a2a8b5743c70a8f2c7ad39521e0d
Lanza <77981833+Lanzadrd@users.noreply.github.com>
2023-05-10 20:04:57 +0200

    Add index.html and styles

--- src/fizzbuzz.ts
+++ src/fizzbuzz.ts
@@ -1,5 +1,28 @@
-console.log("hola g");
+const button = document.querySelector(".input-button")!;
+let screen = document.querySelector(".result")?.innerHTML;
 
-const x: number = 21;
+export const fizzbuzz = (start: number, end: number) => {
+  const filtered = [];
 
-console.log(x);
+  for (let i = start; i <= end; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      filtered.push("FizzBuzz");
+    } else if (i % 3 === 0) {
+      filtered.push("Fizz");
+    } else if (i % 5 === 0) {
+      filtered.push("Buzz");
+    } else {
+      filtered.push(i);
+    }
+  }
+
+  screen = filtered.toString();
+  return filtered;
+};
+
+const startV = document.getElementById("#start");
+const endV = document.getElementById("#end");
+
+button.addEventListener("click", () => {
+  fizzbuzz(startV, endV);
+});
